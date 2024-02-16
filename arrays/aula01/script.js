6f6dc27e7e62a07190cf3d8725f6a05b72eca420
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-02-16 20:58:43 -0300

    Aula 01 - Revisão de arrays

---
+++ arrays/aula01/script.js
@@ -0,0 +1,22 @@
+// Arrays -> Revisão
+// const nomes = ['Eduardo', 'João', 'Maria'];
+// Outra forma de declaração de arrays
+// const nomes = new Array('Eduardo', 'João', 'Maria');
+
+// const novo = [...nomes];
+// novo.pop();
+
+// console.log(nomes);
+// console.log(novo);
+// nomes.unshift(10);
+// nomes.unshift('Jeová');
+// console.log(nomes);
+
+// const nomes = ['André', 'Ana', 'José', 'Bento', 'Raimundo', 'Carla'];
+// const novo = nomes.slice(0, -4)
+// console.log(novo);
+
+// const nome = 'Luis Felipe Souza';
+const nomes = [ 'Luis', 'Felipe', 'Souza' ];
+const nome = nomes.join(' ')
+console.log(nome);
