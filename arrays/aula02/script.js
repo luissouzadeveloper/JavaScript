5c5c640f9e4e56042c91897637e32ff72a076853
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-02-19 10:22:46 -0300

    Aula 02 - Método splice

---
+++ arrays/aula02/script.js
@@ -0,0 +1,14 @@
+// Método splice
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+// Remover
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0, 2);
+
+// push
+// nomes.splice(nomes.length, 0, 'Raimundo');
+
+// unshift
+nomes.splice(0, 0, 'Zé');
+console.log(nomes);
