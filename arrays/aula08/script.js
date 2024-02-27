f861b5b7026dfca230c9122fbee78c510da60daf
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-02-26 21:01:40 -0300

    Aula 08 - forEach

---
+++ arrays/aula08/script.js
@@ -0,0 +1,12 @@
+//forEach
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let total = 0
+array1.forEach((valor) => {
+    total += valor
+})
+
+console.log(total);
+
+// for(let valor of array1){
+//     console.log(valor);
+// };
